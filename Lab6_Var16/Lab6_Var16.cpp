ea191c8996c56e08af7ca679a95f22488f183c80
Алексей <alexey.demidchenko@lll.kpi.ua>
2020-12-09 09:19:00 +0200

    Add project files.

---
+++ Lab6_Var16/Lab6_Var16.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = n; i <= 2*n - 2; i++)
+    {
+        if (isPrime(i) && isPrime(i + 2)) cout << i << " " << i + 2 << endl;
+    }
+    return 0;
+}
