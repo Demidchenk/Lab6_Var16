ef3d5c9cd9324c1e304b512c6a82959655d844a7
Demidchenk <71965474+Demidchenk@users.noreply.github.com>
2021-01-19 10:01:54 +0200

    Update Lab6_Var16.cpp

--- Lab6_Var16/Lab6_Var16.cpp
+++ Lab6_Var16/Lab6_Var16.cpp
@@ -1,14 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 
-bool isPrime(int n)
-{
-    for (int i = 2; i <= n / 2; i++)
-    {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
+bool isPrime(int);
 
 int main()
 {
@@ -19,4 +12,13 @@ int main()
         if (isPrime(i) && isPrime(i + 2)) cout << i << " " << i + 2 << endl;
     }
     return 0;
+}
+
+bool isPrime(int n)
+{
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
